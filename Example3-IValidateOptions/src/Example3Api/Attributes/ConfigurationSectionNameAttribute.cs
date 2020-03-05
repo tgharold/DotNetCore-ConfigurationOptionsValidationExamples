a189145f709fbc62ff59950d9c15e8feb68c27fb
Thomas Harold <tgharold@gmail.com>
2020-03-05 10:09:58 -0500

    Add a note about section names being optional

--- Example3-IValidateOptions/src/Example3Api/Attributes/ConfigurationSectionNameAttribute.cs
+++ Example3-IValidateOptions/src/Example3Api/Attributes/ConfigurationSectionNameAttribute.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace Example3Api.Attributes
 {
+    /// <summary>Used when the section name in the configuration file does not match the
+    /// class name of the options/configuration object.</summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class ConfigurationSectionNameAttribute : Attribute
     {
