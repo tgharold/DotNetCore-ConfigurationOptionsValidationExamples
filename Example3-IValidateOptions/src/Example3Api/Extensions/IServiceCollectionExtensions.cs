d8e54c96ee203de4ff6fcbc084bcf636e643f0f6
Thomas Harold <tgharold@gmail.com>
2020-03-05 08:13:51 -0500

    Assume section name matches class name if no attribute

--- Example3-IValidateOptions/src/Example3Api/Extensions/IServiceCollectionExtensions.cs
+++ Example3-IValidateOptions/src/Example3Api/Extensions/IServiceCollectionExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Reflection;
 using Example3Api.Attributes;
 using Microsoft.Extensions.Configuration;
@@ -12,39 +11,44 @@ namespace Example3Api.Extensions
         public static T ConfigureAndValidateSection<T, TValidator>(
             this IServiceCollection services,
             IConfiguration configuration
-            ) 
+            )
             where T : class
             where TValidator : class, IValidateOptions<T>
         {
-            var sectionName = typeof(T).GetCustomAttribute<ConfigurationSectionNameAttribute>()?.SectionName
-                ?? throw new ArgumentNullException(nameof(ConfigurationSectionNameAttribute));
-            
+            var sectionName = GetSectionName<T>();
+
             var configurationSection = configuration.GetSection(sectionName);
 
             services.AddOptions<T>()
                 .Bind(configurationSection);
-            
+
             services.AddSingleton<IValidateOptions<T>, TValidator>();
-        
+
             return configurationSection.Get<T>();
         }
 
         public static T ConfigureSection<T>(
             this IServiceCollection services,
             IConfiguration configuration
-            ) 
+            )
             where T : class
         {
-            var sectionName = typeof(T).GetCustomAttribute<ConfigurationSectionNameAttribute>()?.SectionName
-                ?? throw new ArgumentNullException(nameof(ConfigurationSectionNameAttribute));
-            
+            var sectionName = GetSectionName<T>();
+
             var configurationSection = configuration.GetSection(sectionName);
 
             services.AddOptions<T>()
                 .Bind(configurationSection);
-            
+
             return configurationSection.Get<T>();
         }
 
+        private static string GetSectionName<T>()
+            where T : class
+        {
+            // Assume that if there is no custom attribute that the section is named after the class
+            return typeof(T).GetCustomAttribute<ConfigurationSectionNameAttribute>()?.SectionName
+                   ?? typeof(T).Name;
+        }
     }
 }
